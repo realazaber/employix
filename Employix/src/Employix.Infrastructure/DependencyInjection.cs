dd3f59110d35ce1b72cb40294786cfb9944ab057
AZ <47402666+realazaber@users.noreply.github.com>
2025-07-05 14:52:59 +0200

    Fix: Changed localhost to host.docker.intenal so that it works with docker

--- Employix/src/Employix.Infrastructure/DependencyInjection.cs
+++ Employix/src/Employix.Infrastructure/DependencyInjection.cs
@@ -9,7 +9,7 @@ namespace Employix.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            string connectionString = "server=localhost;database=employix;User=admin;Password=root1235*";
+            string connectionString = "server=host.docker.internal;database=employix;User=admin;Password=root1235*";
 
             services.AddDbContext<Data.AppDbContext>(options =>
                     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
