a5d3d8559209fb673367772df57b73dcb068b361
AZ <47402666+realazaber@users.noreply.github.com>
2025-07-19 22:30:54 +0200

    Fix: Scripts for managing migrations and db work now (fixed mis match package versions in projects),

--- Employix/CreateMigration.sh
+++ Employix/CreateMigration.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export DB_CONNECTION="server=localhost;port=3306;database=employix;user=admin;password=root1235*"
+
 read -p "Enter migration name: " name
 
 dotnet ef migrations add "$name" \
--- Employix/ResetDb.sh
+++ Employix/ResetDb.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+read -p "Press [Enter] to update the database, or Ctrl+C to cancel..."
+
 export DB_CONNECTION="server=localhost;port=3306;database=employix;user=admin;password=root1235*"
 
-read -p "Press [Enter] to update the database, or Ctrl+C to cancel..."
+dotnet ef database drop \
+  --project ./src/Employix.Infrastructure \
+  --startup-project ./src/Employix.Presentation/Employix.Presentation
 
 dotnet ef database update \
   --project ./src/Employix.Infrastructure \
   --startup-project ./src/Employix.Presentation/Employix.Presentation
+
