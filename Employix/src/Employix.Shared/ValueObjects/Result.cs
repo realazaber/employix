afd0ba55f4e915babb9d252a5c2d6123035ea5ab
AZ <47402666+realazaber@users.noreply.github.com>
2025-08-19 12:34:21 +0200

    Util: Result object to use instead of throwing exceptions,

---
+++ Employix/src/Employix.Shared/ValueObjects/Result.cs
@@ -0,0 +1,34 @@
+﻿namespace Employix.Shared.ValueObjects
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string Message { get; }
+
+        protected Result(bool successful, string message)
+        {
+            IsSuccess = successful;
+            Message = message;
+        }
+
+        public static Result Success(string message) => new Result(true, message);
+        public static Result Failure(string message) => new Result(false, message);
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; }
+
+        private Result(bool successful, string message, T value = default!)
+            : base(successful, message)
+        {
+            Value = value!;
+        }
+
+        public static Result<T> Success(T value, string message)
+            => new Result<T>(true, message, value);
+
+        public static new Result<T> Failure(string message)
+            => new Result<T>(false, message);
+    }
+}
