5950af78e720fbbefd709f46a1c0c96002310060
AZ <47402666+realazaber@users.noreply.github.com>
2025-08-22 14:43:07 +0200

    Tests - E2E: Config

---
+++ Employix/src/Employix.Presentation/Employix.Presentation/wwwroot/E2E Tests/playwright.config.js
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+    use: {
+        baseURL: 'https://localhost:8086/',
+        ignoreHTTPSErrors: true,
+        headless: false
+    }
+});
