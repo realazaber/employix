f56194faa45668f66eb48af188b5254b43925e68
AZ <47402666+realazaber@users.noreply.github.com>
2025-08-22 14:43:36 +0200

    Tests - E2E: Profile navigation links.

---
+++ Employix/src/Employix.Presentation/Employix.Presentation/wwwroot/E2E Tests/Admin/ProfileNavigation.test.js
@@ -0,0 +1,21 @@
+﻿const { test, expect } = require('@playwright/test');
+
+
+test('AdminProfileNavigation', async ({ page }) => {
+    await page.goto("Account/Login");
+    await page.getByRole('textbox', { name: 'name@example.com' }).click();
+    await page.getByRole('textbox', { name: 'name@example.com' }).fill('admin@employix.com');
+    await page.getByRole('textbox', { name: 'name@example.com' }).press('Tab');
+    await page.getByRole('textbox', { name: 'password' }).fill('Admin1235*');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.waitForTimeout(1500);
+    await page.getByRole('button', { name: 'Alex' }).click();
+    await page.getByRole('link', { name: 'Email' }).click();
+    await page.getByRole('link', { name: 'Personal data' }).click();
+    await page.getByRole('link', { name: 'Password' }).click();
+    await page.getByRole('link', { name: 'Two-factor authentication' }).click();
+    await page.getByRole('link', { name: 'Profile' }).click();
+    await page.getByRole('link', { name: 'Password' }).click();
+    await page.getByRole('link', { name: 'Personal data' }).click();
+    await page.getByRole('link', { name: 'Two-factor authentication' }).click();
+});
