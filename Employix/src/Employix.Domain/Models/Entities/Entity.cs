dae219bcafbca34870f11ddff6b5a0568536011b
AZ <47402666+realazaber@users.noreply.github.com>
2025-08-22 14:41:58 +0200

    Feature: Date properties for all entities

--- Employix/src/Employix.Domain/Models/Entities/Entity.cs
+++ Employix/src/Employix.Domain/Models/Entities/Entity.cs
@@ -3,5 +3,9 @@
     public abstract class Entity
     {
         public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
