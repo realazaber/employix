f155c33dc739cc384478cdb9d7f4d39983246bce
AZ <47402666+realazaber@users.noreply.github.com>
2025-08-22 14:41:21 +0200

    Feature: Breadcrumbs

---
+++ Employix/src/Employix.Shared/ValueObjects/Breadcrumb.cs
@@ -0,0 +1,14 @@
+﻿namespace Employix.Shared.ValueObjects
+{
+    public class Breadcrumb
+    {
+        public Breadcrumb(string title)
+        {
+            Title = title;
+            Url = "/" + title.ToLower().Replace(" ", "-");
+        }
+        public string Title { get; set; } = "";
+
+        public string Url { get; set; } = "";
+    }
+}
