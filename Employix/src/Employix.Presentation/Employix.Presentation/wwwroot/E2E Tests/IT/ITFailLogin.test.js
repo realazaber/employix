3a805d72c84c61c0a69042d1bf7f1d19307741cd
AZ <47402666+realazaber@users.noreply.github.com>
2025-08-22 14:44:00 +0200

    Tests - E2E:Wrong password

---
+++ Employix/src/Employix.Presentation/Employix.Presentation/wwwroot/E2E Tests/IT/ITFailLogin.test.js
@@ -0,0 +1,11 @@
+﻿import { test, expect } from '@playwright/test';
+
+test('ITFailLogin', async ({ page }) => {    
+    await page.goto("Account/Login");
+    await page.getByRole('textbox', { name: 'name@example.com' }).click();
+    await page.getByRole('textbox', { name: 'name@example.com' }).fill('okenobi@employix.com');
+    await page.getByRole('textbox', { name: 'name@example.com' }).press('Tab');
+    await page.getByRole('textbox', { name: 'password' }).fill('ObiWan1235');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByText('Error: Invalid login attempt.').click();
+});
